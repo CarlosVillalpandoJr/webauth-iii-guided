3e96562bf80d9a33945e871d16a8c02efe723f45
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2020-01-05 19:58:48 -0600

    working on adding role functionality

--- users/users-router.js
+++ users/users-router.js
@@ -4,6 +4,8 @@ const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
 router.get('/', restricted, (req, res) => {
+  // if the user is an admin they can see all users
+  // if the user isn't an admin, they can only see themselves
   Users.find()
     .then(users => {
       res.json(users);
